c20a137c88659aa5769109738ad21305ad452861
Sebastian <sebas60621@gmail.com>
2023-08-27 14:19:22 -0500

    Urls de la App MoralGames

--- MoralGamesApp/urls.py
+++ MoralGamesApp/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from MoralGamesApp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("MoralGamesApp.urls")),
+    path('', views.home, name="home")
 ]
+
+urlpatterns += staticfiles_urlpatterns()

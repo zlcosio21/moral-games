37d2df28fc8efdb7ba46a3c4ec0b45c55e2c513b
Sebastian <sebas60621@gmail.com>
2023-08-27 15:43:34 -0500

    Creando las vistas para las secciones del sitio web

--- MoralGamesApp/views.py
+++ MoralGamesApp/views.py
@@ -2,5 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-
     return render(request, "MoralGamesApp/home.html")
+
+def contacto(request):
+    return render(request, "MoralGamesApp/contacto.html")
+
+
+def blog(request):
+    return render(request, "MoralGamesApp/blog.html")
+
+
+def servicios(request):
+    return render(request, "MoralGamesApp/servicios.html")
+
+
+def tienda(request):
+    return render(request, "MoralGamesApp/tienda.html")

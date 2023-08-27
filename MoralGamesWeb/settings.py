c821e909a0cb4b6e8e450b1aa3cb23b4b34a08dd
Sebastian <sebas60621@gmail.com>
2023-08-27 14:18:03 -0500

    Instalando App MoralGamesApp y confiurando bootstrap

--- MoralGamesWeb/settings.py
+++ MoralGamesWeb/settings.py
@@ -11,9 +11,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATICFILES_DIRS = [
+
+    os.path.join(BASE_DIR, 'static')
+
+]
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'MoralGamesApp',
 ]
 
 MIDDLEWARE = [

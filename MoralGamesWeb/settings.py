8e07a065020f9e18c6f707a096e60ac646e41db7
Sebastian <sebas60621@gmail.com>
2023-08-28 22:24:54 -0500

    Configurando BBDD a Mysql

--- MoralGamesWeb/settings.py
+++ MoralGamesWeb/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'MoralGamesWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'moral_games',
+        'USER': 'root',
+        'PASSWORD': 'minato21',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 

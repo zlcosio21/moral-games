028cea986895e4a8e704877ad09efd94e3a6316a
Sebastian <sebas60621@gmail.com>
2023-08-29 13:30:37 -0500

    Envio del formulario de contacto al correo

--- contacto/views.py
+++ contacto/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail, EmailMessage
+import os
+from dotenv import load_dotenv
+
+# Email and password private
+load_dotenv()
+EMAIL = os.getenv("EMAIL")
 
 # Create your views here.
 def contacto(request):
+    if request.method == "POST":
+        nombre = request.POST.get("name")
+        email = request.POST.get("email")
+        mensaje = request.POST.get("message")
+
+        envio_email = EmailMessage("Mensaje desde DjangoWebProject", "El usuario con nombre {} con la direccion {} escribe lo siguiente: \n\n {} ".format(nombre, email, mensaje), "",[EMAIL], reply_to=[email])
+
+        try:
+            envio_email.send() 
+            return redirect("/contacto/?valido")
+        except:
+            return redirect("/contacto/?novalido")
 
     return render(request, "contacto/contacto.html")

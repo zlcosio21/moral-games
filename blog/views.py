0d96bd5998263600cbe5a1d59d0f179ee93eadb5
Sebastian <sebas60621@gmail.com>
2023-08-29 23:15:34 -0500

    Llamando a todos los registros de la tabla post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 def blog(request):
-    return render(request, "blog/blog.html")
+
+    posts = Post.objects.all()
+
+    return render(request, "blog/blog.html", {"posts":posts})

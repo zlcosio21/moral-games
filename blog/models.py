254cfbc51d8e99580817423f5ccd7f30b9a577d5
Sebastian <sebas60621@gmail.com>
2023-08-29 20:37:32 -0500

    Creando los models para los posts del blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=50)
+    imagen = models.ImageField(upload_to='blog', null=True, blank=True)
+    contenido = models.CharField(max_length=200)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class meta:
+        verbose_name = "post"
+        plural_name = "posts"
+
+    def __str__(self):
+        return self.titulo

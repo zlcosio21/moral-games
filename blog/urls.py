a0c8f8c15d370179856742e674ce42b8075f7267
Sebastian <sebas60621@gmail.com>
2023-08-29 20:22:20 -0500

    Creando la vista blog y su url

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from blog import views
+
+urlpatterns = [
+
+    path('', views.blog, name="blog"),
+        
+]
+
+urlpatterns += staticfiles_urlpatterns()

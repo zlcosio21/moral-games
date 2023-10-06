f5f681c58dc06c00caea87028810e3c7d39a739d
Sebastian <sebas60621@gmail.com>
2023-10-05 23:13:07 -0500

    Mensajes de error y verificando si el registro es valido

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 from django.core.mail import EmailMessage, send_mail
 import os
 from dotenv import load_dotenv
+from validators import *
+from django.contrib import messages
+from django.contrib.auth import login
 
 # Email and password private
 load_dotenv()
@@ -10,14 +13,22 @@ EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
 
 # Create your views here.
-def autenticacion(request):
+def registro(request):
     if request.method == "POST":
-        username = request.POST.get("name")
+        username = request.POST.get("username")
         email = request.POST.get("email")
         password = request.POST.get("password")
         password_confirm = request.POST.get("password_confirm")
 
-        if password == password_confirm:
+        user = request.user
+        exist = User.objects.filter(username=username).exclude(pk=user.pk).exists()
+
+        username_exist(request, username)
+        username_characters_error(request, username)
+        characters_error(request, password)
+        equals_error(request, password, password_confirm)
+
+        if isvalid(request, username, password, password_confirm):
 
            user = User.objects.create_user(username=username, email=email, password=password)
            user.save()
@@ -25,9 +36,8 @@ def autenticacion(request):
            envio_email = EmailMessage("Mensaje desde MoralGames", "El usuario con nombre {}, ha registrado su cuenta, con correo {} \n\n ".format(username, email), "",[EMAIL], reply_to=[email])
            envio_email.send()
 
-           return redirect("home")
-        
-        else:
-            return redirect("/autenticacion/?novalido")
+           login(request, user)
 
-    return render(request, "autenticacion/registro.html")
+           return redirect("home")
+
+    return render(request, "autenticacion/registro.html", {'messages': messages.get_messages(request)})

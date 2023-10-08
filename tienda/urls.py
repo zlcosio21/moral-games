5195306f58a7983a84985f649a230d2c4cec2f22
Sebastian <sebas60621@gmail.com>
2023-10-07 22:05:15 -0500

    Creacion vista y url de la app tienda

---
+++ tienda/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from tienda import views
+from django.conf import settings
+from django.conf.urls.static import static 
+
+urlpatterns = [
+
+    path('', views.tienda, name="tienda"),
+        
+]

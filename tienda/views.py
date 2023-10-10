d5377efa9be98eddda75caf3d5b37701f5f53ca4
Sebastian <sebas60621@gmail.com>
2023-10-10 16:25:06 -0500

    Buscando video del videojuego mediante su nombre

--- tienda/views.py
+++ tienda/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from inventario.models import Genero, Plataforma, Videojuego
 import os
-from tienda.video import *
+from tienda.video import video
 
 # Create your views here.
 def tienda(request):
@@ -9,10 +9,9 @@ def tienda(request):
 
     return render(request, "tienda/tienda.html", {"videojuegos":videojuego})
 
-def compra(request):
-    videojuego = Videojuego.objects.get(id=4)
+def compra(request, videojuego):
+    videojuego = Videojuego.objects.get(nombre__iexact=videojuego)
     nombre_videojuego = f"{videojuego.nombre} Official Trailer"
-
     video_url = video(nombre_videojuego)
 
-    return render(request, "tienda/compra.html", {"video_url":video_url})
+    return render(request, "tienda/compra.html", {"video_url":video_url, "videojuego":videojuego})

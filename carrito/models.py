25b0a16649d263351b369cc63daf6b6018a0945a
Sebastian <sebas60621@gmail.com>
2023-10-17 22:24:41 -0500

    Realizando cambios en los campos del modelo carrito

--- carrito/models.py
+++ carrito/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Carrito(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    videojuego = models.ForeignKey(Videojuego, on_delete=models.CASCADE)
+    videojuego = models.CharField(max_length=75, unique=True)
     cantidad = models.PositiveSmallIntegerField(default=1)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Usuario {self.usuario} - Producto {self.videojuego.nombre} - {self.cantidad} unidades"
+        return f"Usuario {self.usuario} - Producto {self.videojuego} - {self.cantidad} unidades"

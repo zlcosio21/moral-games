2a595c490cfe77ccea0c48b4a41e4b07de1da323
Sebastian <sebas60621@gmail.com>
2023-10-18 22:33:13 -0500

    Corrigiendo la vista del carrito

--- carrito/views.py
+++ carrito/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from carrito.models import Carrito
 from tienda.models import HistorialCompra
+from inventario.models import Videojuego
 
 # Create your views here.
 def carrito(request, videojuego):
+    videojuego = Videojuego.objects.get(nombre=videojuego)
+
     carrito = Carrito.objects.get_or_create(usuario=request.user, videojuego=videojuego)
     carrito = Carrito.objects.filter(usuario=request.user)
-    historial = HistorialCompra.objects.filter(usuario=request.user)
 
-    return render(request, "carrito/prueba.html", {"carrito":carrito, "historial":historial})
+    return render(request, "carrito/carrito.html", {"carrito":carrito})

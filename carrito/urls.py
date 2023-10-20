78400a2173bd1d5f0f08e0a81f3825af47d6b835
Sebastian <sebas60621@gmail.com>
2023-10-19 23:56:37 -0500

    Vista y url para vaciar el carrito de compras

--- carrito/urls.py
+++ carrito/urls.py
@@ -5,5 +5,6 @@ from django.conf import settings
 urlpatterns = [
 
     path('carrito/<str:videojuego>/', views.carrito, name="carrito"),
+    path('vaciar_carrito', views.vaciar_carrito, name="vaciar_carrito"),
         
 ]
--- carrito/views.py
+++ carrito/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from carrito.models import Carrito
 from tienda.models import HistorialCompra
 from inventario.models import Videojuego
+from django.contrib import messages
 
 # Create your views here.
 def carrito(request, videojuego):
@@ -9,5 +10,14 @@ def carrito(request, videojuego):
 
     carrito = Carrito.objects.get_or_create(usuario=request.user, videojuego=videojuego)
     carrito = Carrito.objects.filter(usuario=request.user)
+
+    return render(request, "carrito/carrito.html", {"carrito":carrito})
+
+def vaciar_carrito(request):
+    carrito = Carrito.objects.filter(usuario=request.user)
+    carrito.delete()
+
+    if not carrito:
+        messages.success(request, "Se ha vaciado el carrito de compras", extra_tags="not_videogames_car")
 
     return render(request, "carrito/carrito.html", {"carrito":carrito})

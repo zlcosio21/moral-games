a69f3b601787999d5ed141d88f0f702a17e78b87
zlcosio21 <sebas60621@gmail.com>
2023-11-21 20:00:50 -0500

    Eliminando los plural names de la app blog

--- blog/models.py
+++ blog/models.py
@@ -9,9 +9,5 @@ class Post(Models):
     contenido = models.CharField(max_length=200)
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    class Meta:
-        verbose_name = "post"
-        plural_name = "posts"
-
     def __str__(self):
         return self.titulo

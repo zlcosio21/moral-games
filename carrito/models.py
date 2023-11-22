6eafd0b610988aad487f78f80e5855929f3f5bdb
zlcosio21 <sebas60621@gmail.com>
2023-11-21 19:55:11 -0500

    Heredando la clase abstracta en la app carrito

--- carrito/models.py
+++ carrito/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 from inventario.models import Videojuego
 from django.contrib.auth.models import User
+from validators import Models
 
 # Create your models here.
-class Carrito(models.Model):
+class Carrito(Models):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     videojuego = models.ForeignKey(Videojuego, on_delete=models.CASCADE)
     cantidad = models.PositiveSmallIntegerField(default=1)
-
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Usuario {self.usuario} - Producto {self.videojuego.nombre} - {self.cantidad} unidades"

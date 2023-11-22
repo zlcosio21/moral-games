41df2b2750a193860733613c2a2a0907a87099cc
zlcosio21 <sebas60621@gmail.com>
2023-11-21 19:51:41 -0500

    Heredando la clase abstracta en la app inventario

--- inventario/models.py
+++ inventario/models.py
@@ -1,25 +1,20 @@
 from django.db import models
+from validators import Models
 
 # Create your models here.
-class Plataforma(models.Model):
+class Plataforma(Models):
     nombre = models.CharField(unique=True, max_length=50, null=False)
-
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Plataforma - {self.nombre}"
 
-class Genero(models.Model):
+class Genero(Models):
     nombre = models.CharField(unique=True, max_length=50, null=False)
-
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Genero - {self.nombre}"
 
-class Videojuego(models.Model):
+class Videojuego(Models):
     nombre = models.CharField(unique=True, max_length=50, null=False)
     imagen = models.ImageField(upload_to='tienda', null=False)
     precio = models.PositiveIntegerField(null=False)
@@ -27,9 +22,6 @@ class Videojuego(models.Model):
     genero = models.ManyToManyField(Genero)
     plataforma = models.ManyToManyField(Plataforma)
     informacion = models.CharField(max_length=300, null=True)
-
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         generos = ", ".join([genero.nombre for genero in self.genero.all()])

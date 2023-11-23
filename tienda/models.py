803bcfd4475e2841c8845de80ff28045eb9e3fc0
zlcosio21 <sebas60621@gmail.com>
2023-11-22 23:08:36 -0500

    Heredando la clase abstracta en la app tienda

--- tienda/models.py
+++ tienda/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from validators import Models
 
 # Create your models here.
-class HistorialCompra(models.Model):
+class HistorialCompra(Models):
     id = models.AutoField(primary_key=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     videojuego = models.CharField(max_length=50)
     cantidad = models.PositiveIntegerField(null=False)
-
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Pedido #{self.id} - {self.usuario} - {self.videojuego} - {self.cantidad} unidades - Realizado {self.created}"

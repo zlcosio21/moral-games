cfcb0f5fddcc2082521e63f5eab5e4bb11284aef
zlcosio21 <sebas60621@gmail.com>
2023-11-25 00:05:02 -0500

    Incluyendo la url de la vista comprar_carrito

--- carrito/urls.py
+++ carrito/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('agregar_al_carrito/<str:videojuego>/', views.agregar_al_carrito, name="agregar_al_carrito"),
     path('vaciar_carrito/', views.vaciar_carrito, name="vaciar_carrito"),
     path('eliminar_del_carrito/<str:videojuego>/', views.eliminar_del_carrito, name="eliminar_del_carrito"),
+    path('comprar_carrito', views.comprar_carrito, name="comprar_carrito")
         
 ]

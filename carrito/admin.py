75dc9b155e5e8fe37d59368419c0951db058f963
zlcosio21 <sebas60621@gmail.com>
2023-11-25 19:40:27 -0500

    Incluyendo el modelo historial del carrito en el admin

--- carrito/admin.py
+++ carrito/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from carrito.models import Carrito
+from carrito.models import Carrito, HistorialCompraCarrito
 
 # Register your models here.
 class CarritoAdmin(admin.ModelAdmin):
 
     readonly_fields = ('created', 'updated')
 
-admin.site.register(Carrito, CarritoAdmin)
+class HistorialCompraCarritoAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('created', 'updated')
+
+admin.site.register(Carrito, CarritoAdmin)    
+admin.site.register(HistorialCompraCarrito, HistorialCompraCarritoAdmin)

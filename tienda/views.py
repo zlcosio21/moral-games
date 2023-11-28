05b89cbe36be1230b57606d6e99d70706623f657
zlcosio21 <sebas60621@gmail.com>
2023-11-28 15:03:08 -0500

    Vista para el historial de compra del usuario

--- tienda/views.py
+++ tienda/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from inventario.models import Videojuego
 from tienda.video import video
-from django.db.models import Q
+from django.db.models import Q, Value, IntegerField
 from django.contrib import messages
 from validators import stock_error, send_email_buy, save_order, validator_stock
+from carrito.models import HistorialCompraCarrito
+from tienda.models import HistorialCompra
 
 # Create your views here.
 def tienda(request):
@@ -40,3 +42,13 @@ def compra(request, videojuego):
     video_url = video(nombre_videojuego)
 
     return render(request, "tienda/compra.html", {"video_url":video_url, "videojuego":videojuego, 'messages': messages.get_messages(request)})
+
+def historial_compra(request):
+    historial_compra = HistorialCompra.objects.filter(usuario=request.user).values("id", "videojuego", "created", "cantidad")
+    
+    historial_compra_carrito = HistorialCompraCarrito.objects.filter(usuario=request.user).values("id", "videojuego", "created")
+    historial_compra_carrito = historial_compra_carrito.annotate(cantidad=Value(None, output_field=IntegerField()))
+
+    historial_completo = historial_compra.union(historial_compra_carrito).order_by("created")
+
+    return render(request, "tienda/historial_compra.html", {"historial_completo": historial_completo})

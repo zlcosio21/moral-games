cd870dbad7778a56ef19913904f851bdc1d40588
zlcosio21 <sebas60621@gmail.com>
2023-12-10 13:53:58 -0500

    deploy

--- MoralGamesWeb/settings.py
+++ MoralGamesWeb/settings.py
@@ -89,12 +89,8 @@ WSGI_APPLICATION = 'MoralGamesWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'moral_games',
-        'USER': 'root',
-        'PASSWORD': 'minato21',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

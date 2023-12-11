443c220f740e2054267694cb293d7b003c17c497
zlcosio21 <sebas60621@gmail.com>
2023-12-10 20:55:28 -0500

    Deplot

--- MoralGamesWeb/settings.py
+++ MoralGamesWeb/settings.py
@@ -138,10 +138,10 @@ EMAIL_HOST_PASSWORD = PASSWORD
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

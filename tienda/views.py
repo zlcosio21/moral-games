8cb8bcb6846f5092e1da34a41c3dc023dd4b6c59
zlcosio21 <sebas60621@gmail.com>
2024-05-21 23:32:43 -0500

    Refactor de las vistas

--- tienda/views.py
+++ tienda/views.py
@@ -1,57 +1,50 @@
 from django.shortcuts import render, redirect
-from inventario.models import Videojuego
-from tienda.video import video
-from django.db.models import Q, Value, IntegerField
-from django.contrib import messages
-from validators import stock_error, send_email_buy, save_order, validator_stock
-from carrito.models import HistorialCompraCarrito
 from tienda.models import HistorialCompra
-import random
+from utils.validators import stock_errors
+from inventario.models import Videojuego
+from utils.emails import send_email_buy
+from utils.purchase_history import *
+from django.contrib import messages
+from tienda.models import Video
+
 
 # Create your views here.
 def tienda(request):
+    videojuegos_random = Videojuego.random_videogames()
+
     if request.method == "POST":
         busqueda = request.POST.get("busqueda")
-
-        videojuegos = Videojuego.objects.filter(
-            Q(nombre__icontains=busqueda) |
-            Q(plataforma__nombre__icontains=busqueda) |
-            Q(genero__nombre__icontains=busqueda)
-        ).distinct()
+        videojuegos = Videojuego.search(busqueda)
 
         return render(request, "tienda/busqueda.html", {"videojuegos":videojuegos, "busqueda":busqueda})
 
-    videojuego = list(Videojuego.objects.all())
-    random.shuffle(videojuego)
+    return render(request, "tienda/tienda.html", {"videojuegos":videojuegos_random})
 
-    return render(request, "tienda/tienda.html", {"videojuegos":videojuego})
 
 def compra(request, videojuego):
     videojuego = Videojuego.objects.get(nombre__iexact=videojuego)
+    video_url = Video.search(f"{videojuego.nombre} Videogame Official Trailer")
 
     if request.method == "POST":
         cantidad = request.POST.get("quantity")
 
-        if stock_error(request, cantidad, videojuego):
+        if stock_errors(request, videojuego, cantidad):
             return redirect("compra", videojuego=videojuego.nombre)
 
-        validator_stock(videojuego, cantidad)
+        videojuego.update_stock(cantidad)
+
+        HistorialCompra.save_order(request, videojuego, cantidad)
+
         send_email_buy(request, videojuego, cantidad)
-        save_order(request, videojuego, cantidad)
 
         messages.success(request, f"Compra realizada exitosamente, factura enviada a su correo", extra_tags="buy_succesfull")
 
-    nombre_videojuego = f"{videojuego.nombre} Official Trailer"
-    video_url = video(nombre_videojuego)
+    return render(request, "tienda/compra.html", {"video_url":video_url, "videojuego":videojuego})
 
-    return render(request, "tienda/compra.html", {"video_url":video_url, "videojuego":videojuego, 'messages': messages.get_messages(request)})
 
 def historial_compra(request):
-    historial_compra = HistorialCompra.objects.filter(usuario=request.user).values("id", "videojuego", "created", "cantidad")
-
-    historial_compra_carrito = HistorialCompraCarrito.objects.filter(usuario=request.user).values("id", "videojuego", "created")
-    historial_compra_carrito = historial_compra_carrito.annotate(cantidad=Value(None, output_field=IntegerField()))
-
-    historial_completo = historial_compra.union(historial_compra_carrito).order_by("created")
+    historial_compra = obtain_purchase_history(request.user)
+    historial_carrito = obtain_car_history(request.user)
+    historial_completo = obtain_complete_history(historial_compra, historial_carrito)
 
     return render(request, "tienda/historial_compra.html", {"historial_completo": historial_completo})

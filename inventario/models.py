a33f132ee634919fedd8b1548288fb8662a4f73a
zlcosio21 <sebas60621@gmail.com>
2024-05-23 22:20:35 -0500

    Nuevos metodos para el modelo Videojuego

--- inventario/models.py
+++ inventario/models.py
@@ -1,5 +1,8 @@
-from django.db import models
 from MoralGamesWeb.base_models import Models
+from django.db.models import Q
+from django.db import models
+import random
+
 
 # Create your models here.
 class Plataforma(Models):
@@ -8,11 +11,13 @@ class Plataforma(Models):
     def __str__(self):
         return f"Plataforma - {self.nombre}"
 
+
 class Genero(Models):
     nombre = models.CharField(unique=True, max_length=50, null=False)
 
     def __str__(self):
         return f"Genero - {self.nombre}"
+
 
 class Videojuego(Models):
     nombre = models.CharField(unique=True, max_length=50, null=False)
@@ -22,6 +27,27 @@ class Videojuego(Models):
     genero = models.ManyToManyField(Genero)
     plataforma = models.ManyToManyField(Plataforma)
     informacion = models.CharField(max_length=300, null=True)
+
+    @classmethod
+    def random_videogames(cls):
+        videojuego = list(cls.objects.all())
+        random.shuffle(videojuego)
+
+        return videojuego
+
+    @classmethod
+    def search(cls, busqueda):
+        videojuegos = cls.objects.filter(
+            Q(nombre__icontains=busqueda) |
+            Q(plataforma__nombre__icontains=busqueda) |
+            Q(genero__nombre__icontains=busqueda)
+        ).distinct()
+
+        return videojuegos
+
+    def update_stock(self, cantidad):
+        self.cantidad -= int(cantidad)
+        self.save()
 
     def __str__(self):
         generos = ", ".join([genero.nombre for genero in self.genero.all()])

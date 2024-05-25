bd4d717ff35e8c3d0420794b6fa664d0bd550300
zlcosio21 <sebas60621@gmail.com>
2024-05-25 11:22:07 -0500

    Mejorando funcionamiento de las vistas

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,40 +1,32 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.core.mail import EmailMessage, send_mail
-import os
-from validators import *
-from django.contrib import messages
+from utils.validators import register_user, register_errors
 from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
+from utils.emails import send_email_register
+from django.contrib import messages
+
 
 # Create your views here.
 def registro(request):
     if request.method == "POST":
+        user = request.user
         username = request.POST.get("username")
         email = request.POST.get("email")
         password = request.POST.get("password")
         password_confirm = request.POST.get("password_confirm")
 
-        user = request.user
-        exist = User.objects.filter(username=username).exclude(pk=user.pk).exists()
+        if register_errors(request, username, email, password, password_confirm):
+            return redirect("registro")
 
-        username_exist(request, username)
-        username_characters_error(request, username)
-        characters_error(request, password)
-        equals_error(request, password, password_confirm)
+        user = register_user(user, username, email, password)
 
-        if isvalid(request, username, password, password_confirm):
+        send_email_register(username, email)
 
-           user = User.objects.create_user(username=username, email=email, password=password)
-           user.save()
+        login(request, user)
 
-           envio_email = EmailMessage("Mensaje desde MoralGames", "El usuario con nombre {}, ha registrado su cuenta, con correo {} \n\n ".format(username, email), "",[EMAIL], reply_to=[email])
-           envio_email.send()
+        return redirect("home")
 
-           login(request, user)
+    return render(request, "autenticacion/registro.html")
 
-           return redirect("home")
-
-    return render(request, "autenticacion/registro.html", {'messages': messages.get_messages(request)})
 
 def inicio_sesion(request):
     if request.method == "POST":
@@ -43,14 +35,16 @@ def inicio_sesion(request):
 
         user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
+        if user == None:
+            messages.error(request, "La cuenta no existe, ingrese los datos nuevamente", extra_tags="account_not_exist")
+            return redirect("inicio_sesion")
 
-            return redirect("home")
+        login(request, user)
 
-        messages.error(request, "La cuenta no existe, ingrese los datos nuevamente", extra_tags="account_not_exist")
+        return redirect("home")
 
-    return render(request, "autenticacion/inicio_sesion.html", {'messages': messages.get_messages(request)})
+    return render(request, "autenticacion/inicio_sesion.html")
+
 
 def cerrar_sesion(request):
     logout(request)

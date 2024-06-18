d6e2fa1a408521b1d222d09abda446e6fd7fe59f
zlcosio21 <sebas60621@gmail.com>
2024-06-17 22:56:16 -0500

    Agregando modelos al panel de admin

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from tienda.models import HistorialCompra
+from tienda.models import HistorialCompra, Video
 
 # Register your models here.
 class HistorialCompraAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
 
+class VideoAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
 
 admin.site.register(HistorialCompra, HistorialCompraAdmin)
+admin.site.register(Video, VideoAdmin)

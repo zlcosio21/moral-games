4c141cc4d42b5a3c0fa8a48e231c9393fe8b1d79
zlcosio21 <sebas60621@gmail.com>
2024-07-05 21:58:12 -0500

    Cambio de nombre al proyecto

---
+++ moral_games/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from django.contrib import admin
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("home.urls")),
+    path("blog/", include("blog.urls")),
+    path("autenticacion/", include("authentication.urls")),
+    path("tienda/", include("store.urls")),
+    path("carrito/", include("cart.urls")),
+]

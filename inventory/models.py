89e786f0f3f2ce383ad92bb9b14dd4d1b6deba11
zlcosio21 <sebas60621@gmail.com>
2024-07-07 01:43:40 -0500

    Agregando metodos al modelo platform

---
+++ inventory/models.py
@@ -0,0 +1,31 @@
+from moral_games.base_models import Models
+from django.db.models import Q
+from django.db import models
+import random
+
+
+# Create your models here.
+class Platform(Models):
+    name = models.CharField(unique=True, max_length=50, null=False)
+
+    @classmethod
+    def get(cls, name):
+        if name is None or not cls.exist(name):
+            return cls.get_random_platform()
+
+        return cls.objects.get(name__iexact=name)
+
+    @classmethod
+    def get_all_platforms(cls):
+        return cls.objects.all().order_by("?")
+
+    @classmethod
+    def get_random_platform(cls):
+        return cls.objects.all().order_by("?").first()
+
+    @classmethod
+    def exist(cls, name):
+        return cls.objects.filter(name__iexact=name).exists()
+
+    def __str__(self):
+        return self.name

a9014c1a1edaadc93dd3a1e74c57e1081ee994a2
zlcosio21 <sebas60621@gmail.com>
2024-07-09 14:01:36 -0500

    Eliminando app para renombrarla a authentication

--- autenticacion/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User
-
-class RegisterUser(User):
-    def create(user, username, email, password):
-        user = User.objects.create_user(username=username, email=email, password=password)
-        user.save()
-
-        return user

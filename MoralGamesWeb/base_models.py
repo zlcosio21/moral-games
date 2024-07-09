093563bb04097f4205e385bfb470e2d821a78389
zlcosio21 <sebas60621@gmail.com>
2024-07-09 14:00:21 -0500

    Eliminando proyecto para renombrarlo a moral_games

--- MoralGamesWeb/base_models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Abstract class
-class Models(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True

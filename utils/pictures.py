891edce291c4bcd2374e94770766cad340a6d031
zlcosio21 <sebas60621@gmail.com>
2024-07-18 00:05:19 -0500

    Refactorizando nombre de archivo de manejo de imagenes

--- utils/pictures.py
+++ utils/pictures.py
@@ -2,10 +2,15 @@ from inventory.models import Videogame
 from blog.models import Post
 
 
-def get_latest_pictures():
+def get_latest_pictures(limit=0):
     posts = Post.get_latest_pictures()
     videogames = Videogame.get_latest_pictures()
 
     pictures = posts.union(videogames).order_by("-created")
 
+    if limit > 0:
+        return pictures[:limit]
+
     return pictures[:6]
+
+

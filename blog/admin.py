b7428f0af44320e4c397a046a75fc5c26ad43368
zlcosio21 <sebas60621@gmail.com>
2024-07-19 00:22:37 -0500

    Refactorizando registro al panel de admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,15 @@
+from .models import Post, Comment
 from django.contrib import admin
-from .models import Post
+
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    readonly_fields = ['created']
+    readonly_fields = ("created", "updated")
+
+
+class CommentAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", "updated")
+
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)

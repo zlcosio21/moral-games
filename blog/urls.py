6d366cc15e063086fb843a77dd9ba52a4c863201
zlcosio21 <sebas60621@gmail.com>
2024-07-19 00:22:00 -0500

    Agregando urls de las vistas

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
 from blog import views
+
 
 urlpatterns = [
 
     path('', views.blog, name="blog"),
+    path('ver_post/<int:id>/', views.watch_post, name="ver_post"),
+    path('agregar_comentario_a_post/<int:id>/', views.add_comment_to_post, name="agregar_comentario_a_post"),
+    path('agregar_comentario_a_videojuego/<int:id>/', views.add_comment_to_videogame, name="agregar_comentario_a_videojuego"),
 
 ]
 

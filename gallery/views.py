f51bb235034436ec5ba564c8e21d05247a917971
zlcosio21 <sebas60621@gmail.com>
2024-07-20 00:19:10 -0500

    Agregando vistas a la nueva app gallery

---
+++ gallery/views.py
@@ -0,0 +1,31 @@
+from utils.pictures import get_latest_pictures
+from inventory.models import Videogame, Genre
+from django.shortcuts import render
+from blog.models import Post
+
+
+# Create your views here.
+def gallery(request):
+    latest_pictures = get_latest_pictures(12)
+    videogames = Videogame.get_random_name_videogames(4)
+    all_genres = Genre.get_all_genres(4)
+
+    context = {
+        "latest_pictures": latest_pictures,
+        "videogames": videogames,
+        "all_genres": all_genres,
+    }
+
+    return render(request, "gallery/gallery.html", context)
+
+
+def watch_gallery_genre(request, id):
+    genre = Genre.get_by_id(id)
+    all_genres = Genre.get_all_genres(4)
+    pictures_videogames = Videogame.get_pictures_by_genre(genre, all=True)
+    pictures_posts = Post.get_pictures_by_genre(genre, all=True)
+    all_pictures = Genre.get_all_pictures(pictures_videogames, pictures_posts)
+
+    context = {"genre": genre, "all_genres": all_genres, "all_pictures": all_pictures}
+
+    return render(request, "gallery/gallery_genre.html", context)

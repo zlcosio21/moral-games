bbe168888e0c9ce5d71b2df3794c27dedbcd9761
zlcosio21 <sebas60621@gmail.com>
2024-07-22 23:58:02 -0500

    Arreglando las urls de todas las aplicaciones

--- moral_games/urls.py
+++ moral_games/urls.py
@@ -3,10 +3,13 @@ from django.contrib import admin
 
 
 urlpatterns = [
+
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
+    path("galeria/", include("gallery.urls")),
     path("blog/", include("blog.urls")),
     path("autenticacion/", include("authentication.urls")),
     path("tienda/", include("store.urls")),
     path("carrito/", include("cart.urls")),
+
 ]
